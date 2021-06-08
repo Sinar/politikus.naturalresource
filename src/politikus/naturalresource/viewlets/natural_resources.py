e3a0bf1070d06a09b8ff03a30a5ea81b16270f1b
Khairil Yusof <khairil.yusof@gmail.com>
2021-06-08 15:04:41 +0800

    Add viewlet to show natural resources related to a concession

--- src/politikus/naturalresource/viewlets/natural_resources.py
+++ src/politikus/naturalresource/viewlets/natural_resources.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from plone.app.layout.viewlets import ViewletBase
+from Acquisition import aq_inner
+from zope.component import getUtility
+from zope.intid.interfaces import IIntIds
+from zope.security import checkPermission
+from zc.relation.interfaces import ICatalog
+from zope.schema.interfaces import IVocabularyFactory
 
 
 class NaturalResources(ViewletBase):
-
-    def update(self):
-        self.message = self.get_message()
-
-    def get_message(self):
-        return u'My message'
 
     def render(self):
         return super(NaturalResources, self).render()
